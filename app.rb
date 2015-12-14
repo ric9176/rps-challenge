2547d4e0b143c03567ccac8517bf8ac0a9603b6f
Richard Moss <richard@englishtutorslive.com>
2015-12-14 12:18:34 +0000

    first user story with sessions

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class Rps < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
- 	post '/names' do 
- 		@name = params[:name]
- 		erb :name
+ 	post '/names' do
+ 	session[:name] = params[:name]
+  redirect '/play'
  	end
+
+  get '/play' do
+    @name = session[:name]
+    erb :play
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
